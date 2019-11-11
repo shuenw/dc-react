d265ac503be32f36c6ad7854a668b6de56fd8959
Dexter Wang <shuenwang@gmail.com>
2019-11-10 21:51:12 -0600

    Finished interactions & animations

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -1,6 +1,24 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
 
-const Header = ({ siteTitle }) => <div></div>
+const Header = ({ siteTitle }) => (
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/">
+        <img
+          src={require('../images/logo-seoul-hanhyeonglee.jpg')}
+          width="50"
+        />
+      </Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/buy">
+        <button>Buy</button>
+      </Link>
+    </div>
+  </div>
+)
 
 export default Header
